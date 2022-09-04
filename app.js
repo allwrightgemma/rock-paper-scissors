1d39b14d38c1ae21fe417dd109f4f943a3f91cba
allwrightgemma <34656602+allwrightgemma@users.noreply.github.com>
2022-09-04 12:53:50 +0100

    Reduced if statements

--- app.js
+++ app.js
@@ -6,39 +6,6 @@ let userChoice;
 let computerChoice;
 let result;
 
-function generateComputerChoice() {
-  let computerChoices = ["rock", "paper", "scissors"];
-  computerChoice =
-    computerChoices[Math.floor(Math.random() * computerChoices.length)];
-  computerChoiceDisplay.innerHTML = computerChoice;
-}
-
-function getResult() {
-  console.log(computerChoice);
-  if (computerChoice === userChoice) {
-    result = "It's a draw!";
-  }
-  if (computerChoice === "rock" && userChoice === "paper") {
-    result = "You win!";
-  }
-  if (computerChoice === "rock" && userChoice === "scissors") {
-    result = "You lose!";
-  }
-  if (computerChoice === "paper" && userChoice === "scissors") {
-    result = "You win!";
-  }
-  if (computerChoice === "paper" && userChoice === "rock") {
-    result = "You lose!";
-  }
-  if (computerChoice === "scissors" && userChoice === "rock") {
-    result = "You win!";
-  }
-  if (computerChoice === "scissors" && userChoice === "paper") {
-    result = "You lose!";
-  }
-  resultDisplay.innerHTML = result;
-}
-
 possibleChoices.forEach((possibleChoice) =>
   possibleChoice.addEventListener("click", (event) => {
     userChoice = event.target.id;
@@ -47,18 +14,25 @@ possibleChoices.forEach((possibleChoice) =>
     getResult();
   })
 );
+function generateComputerChoice() {
+  let computerChoices = ["rock", "paper", "scissors"];
+  computerChoice =
+    computerChoices[Math.floor(Math.random() * computerChoices.length)];
+  computerChoiceDisplay.innerHTML = computerChoice;
+}
 
-// function generateComputerChoice() {
-//   const randomNumber = Math.floor(Math.random() * possibleChoices.length) + 1;
+function getResult() {
+  if (computerChoice === userChoice) {
+    result = "It's a draw!";
+  } else if (computerChoice === "rock" && userChoice === "paper") {
+    result = "You win!";
+  } else if (computerChoice === "paper" && userChoice === "scissors") {
+    result = "You win!";
+  } else if (computerChoice === "scissors" && userChoice === "rock") {
+    result = "You win!";
+  } else {
+    result = "You lose!";
+  }
 
-//   if (randomNumber === 1) {
-//     computerChoice = "rock";
-//   }
-//   if (randomNumber === 2) {
-//     computerChoice = "paper";
-//   }
-//   if (randomNumber === 3) {
-//     computerChoice = "scissors";
-//   }
-//   computerChoiceDisplay.innerHTML = computerChoice;
-// }
+  resultDisplay.innerHTML = result;
+}
