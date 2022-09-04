e0934206e0bd0d45e01fc3cc28ed34146ff2441c
allwrightgemma <34656602+allwrightgemma@users.noreply.github.com>
2022-09-04 16:53:25 +0100

    Added styling

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const computerChoiceDisplay = document.getElementById("computer-choice");
 const userChoiceDisplay = document.getElementById("user-choice");
 const resultDisplay = document.getElementById("result");
-let possibleChoices = document.querySelectorAll(".choice");
+let possibleChoices = document.querySelectorAll("button", "i");
 let userChoice;
 let computerChoice;
 let result;
