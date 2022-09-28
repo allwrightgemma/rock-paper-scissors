b7c9da15932ea3fc51481c5ac539a11e542ada22
allwrightgemma <34656602+allwrightgemma@users.noreply.github.com>
2022-09-28 17:07:27 +0100

    Added scoring functions

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const computerChoiceDisplay = document.getElementById("computer-choice");
 const userChoiceDisplay = document.getElementById("user-choice");
 const resultDisplay = document.getElementById("result");
+const userScoreDisplay = document.getElementById("user-score");
+const compScoreDisplay = document.getElementById("comp-score");
 let possibleChoices = document.querySelectorAll("button", "i");
 let userChoice;
 let computerChoice;
 let result;
+let userScore = 0;
+let compScore = 0;
 
 possibleChoices.forEach(function (possibleChoice) {
   possibleChoice.addEventListener("click", handleUserChoice);
@@ -25,17 +29,38 @@ function generateComputerChoice() {
   computerChoiceDisplay.innerHTML = computerChoice;
 }
 
+function win() {
+  userScore++;
+  userScoreDisplay.innerHTML = userScore;
+  compScoreDisplay.innerHTML = compScore;
+}
+
+function lose() {
+  compScore++;
+  userScoreDisplay.innerHTML = userScore;
+  compScoreDisplay.innerHTML = compScore;
+}
+
+function draw() {
+  userScoreDisplay.innerHTML = userScore;
+  compScoreDisplay.innerHTML = compScore;
+}
 function getResult() {
   if (computerChoice === userChoice) {
     result = "It's a draw!";
   } else if (computerChoice === "rock" && userChoice === "paper") {
     result = "You win!";
+    win();
   } else if (computerChoice === "paper" && userChoice === "scissors") {
     result = "You win!";
+    win();
   } else if (computerChoice === "scissors" && userChoice === "rock") {
     result = "You win!";
+    win();
   } else {
     result = "You lose!";
+    lose();
   }
+
   resultDisplay.innerHTML = result;
 }
